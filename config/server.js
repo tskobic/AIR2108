5e04510412b016041df1e7ef41666d9323d712ae
tcicak <ctomislav0@gmail.com>
2021-11-28 00:08:00 +0100

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f3702256b1405ceb791f10e8503d77f6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '97248e73-0af1-4425-bf8e-73922fca4e83'
+};
