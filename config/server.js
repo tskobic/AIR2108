b37cd5e114e32d5bbdd30da90dd37b3d372a2a2d
tskobic <tskobic@foi.hr>
2021-12-23 02:31:10 +0100

    STRAPI AZURE UPLOAD PROVIDER: - azure upload provider implemented and plugin installed

--- config/server.js
+++ config/server.js
@@ -5,5 +5,8 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'f3702256b1405ceb791f10e8503d77f6'),
     },
+    watchIgnoreFiles: [
+      '**/AIR2108/qr.svg**',
+    ]
   },
 });
