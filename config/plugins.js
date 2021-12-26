43b5ad3fdc49df6defd99f2ce90a661e49cd0567
tcicak <ctomislav0@gmail.com>
2021-12-26 21:46:34 +0100

    Provider i email config promjene

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,37 @@
 'use strict';
 
 module.exports = ({ env }) => ({
-  // ...
+  /*
   email: {
     config: {
       provider: 'sendgrid',
       providerOptions: {
-        apiKey: env('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'),
+        apiKey: env('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'),
         //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
         //
       },
       settings: {
-        defaultFrom: 'invappstrapi@gmail.com',
-        defaultReplyTo: 'invappstrapi@gmail.com',
+        defaultFrom: 'strapitesting@gmail.com',
+        defaultReplyTo: 'strapitesting@gmail.com',
+      },
+    },
+  },
+*/
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.ethereal.email'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('judge.doyle49@ethereal.email'),
+          pass: env('8vJqWzzVYCRTGqUvub'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: 'judge.doyle49@ethereal.email',
+        defaultReplyTo: 'judge.doyle49@ethereal.email',
       },
     },
   },
